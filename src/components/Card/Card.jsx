44b7905c72401364f389b9d406d8ba3add0c91b6
Audrius Ausauskas <aaaudrius@gmail.com>
2022-12-28 09:45:23 +0200

    complete upperContainer

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,6 +6,13 @@ const Card = () => {
     <div className={style.card}>
       <div className={style.topContainer}>
         <div className={style.screen}></div>
+        <div className={style.textBox}>
+          <h3 className={style.header}>
+            Lorem ipsum dolor sit amet, consectetur
+          </h3>
+          <h4 className={style.subheader}>Lorem ipsum</h4>
+          <p className={style.text12}>Lorem ipsum dolor sit</p>
+        </div>
       </div>
       <div className={style.bottomContainer}></div>
     </div>
